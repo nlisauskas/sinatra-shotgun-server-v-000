b3714af2eab31579f94d7e088b24611729404cc7
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-14 12:09:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! Nick built this!!!! "
+    "Started my server using shotgun!! "
   end
 
 end
